e3a526fcf38dbfd70b118f0cd39b1247d58bc2c2
AbdouAzizFall <fallaziz699@gmail.com>
2024-08-14 18:55:36 +0000

    First day

---
+++ src/main/java/com/example/formulaire/sn/icagi/entities/User.java
@@ -0,0 +1,2 @@
+package com.example.formulaire.sn.icagi.entities;public class User {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.formulaire {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.formulaire to javafx.fxml;
+    exports com.example.formulaire;
+}
